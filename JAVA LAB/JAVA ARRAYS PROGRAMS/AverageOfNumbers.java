0e2180515e774b505a34b6ba48365efbedde3ccf
Manasvi Sripada <85676177+20WH5A1203-Manasvi@users.noreply.github.com>
2021-07-08 10:06:15 +0530

    Add files via upload

---
+++ JAVA LAB/JAVA ARRAYS PROGRAMS/AverageOfNumbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class  AverageOfNumbers 
+{
+        public static void main(String[] args) 
+        {
+        System.out.println("Enter total Numbers :");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        double[] arr = new double[n];
+        double total = 0;
+        for(int i=0; i<arr.length; i++){
+        	System.out.print("Enter Element No."+(i+1)+": ");
+        	arr[i] = scanner.nextDouble();
+        }
+        scanner.close();
+        for(int i=0; i<arr.length; i++){
+        	total = total + arr[i];
+        }
+        double average = total / arr.length;
+        
+        System.out.format("The average is: %.3f", average);
+    }
+}
